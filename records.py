e44181fb1740b3093116da9f847d308e55b991ad
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2015-11-12 23:12:04 +0000

    Separated record processing from work on sequences.

---
+++ records.py
@@ -0,0 +1,50 @@
+# Record processing
+
+import csv
+
+from . import logging
+
+
+def read_records_from_csv(
+        file, record_constructor=None, commentchar='#'):
+    """Reads records from a CSV-formatted file.
+
+    Discards comment lines (ones that start with the given comment
+    character) and whitespace lines.  Logs errors rather than
+    propagating exceptions.  If given, the record constructor is
+    responsible for parsing, validating, and converting the given fields
+    and assembling them into a record.  (It may throw exceptions.)  It
+    essentially defines a schema on the file.  If a record constructor
+    is not given, the raw CSV records (lists of strings) are returned.
+    """
+    logger = logging.getLogger(__name__).getChild(
+        'read_records_from_csv')
+    rownum = 1
+    num_bad = 0
+    for rawrow in csv.reader(file):
+        # Discard empty rows
+        if rawrow is None or len(rawrow) == 0:
+            continue
+        # Discard whitespace and comments
+        first = rawrow[0].lstrip()
+        if ((len(rawrow) == 1 and len(first) == 0) or
+                first.startswith(commentchar)):
+            continue
+        # Parse, validate, construct
+        try:
+            row = (record_constructor(rawrow)
+                   if record_constructor is not None else rawrow)
+        except Exception as e:
+            logger.exception(
+                    'Discarding bad record {}: {}', rownum, rawrow)
+            num_bad += 1
+        else:
+            # Return the row if not None
+            if row is not None:
+                yield row
+                rownum += 1
+    # Log results of reading
+    num_records = rownum - 1
+    logger.info(
+            'Processed {}/{} records.  Discarded {}/{} records.',
+            num_records - num_bad, num_records, num_bad, num_records)
---
+++ test/records_test.py
@@ -0,0 +1,67 @@
+# Tests records.py.
+
+import io
+import unittest
+
+from .. import logging
+from .. import records
+
+
+class ReadRecordsFromCsvTest(unittest.TestCase):
+
+    # CSV records that could be interpreted as events
+    text = """
+    # Comment, empty line above
+    129, 75,9, a , yes
+    3917,
+    3791
+    ,6160,7,9,,on
+
+    # Valid events below
+    9650, 67, f
+    6406,32,d
+    3128 , 75 , 7 , c , true
+    755,97,e,y
+    214,81,0,c,1.0
+    """ # Whitespace-only non-empty line
+
+    raw_records = (
+        ['    129', ' 75', '9', ' a ', ' yes'],
+        ['    3917', ''],
+        ['    3791'],
+        ['    ', '6160', '7', '9', '', 'on'],
+        ['    9650', ' 67', ' f'],
+        ['    6406', '32', 'd'],
+        ['    3128 ', ' 75 ', ' 7 ', ' c ', ' true'],
+        ['    755', '97', 'e', 'y'],
+        ['    214', '81', '0', 'c', '1.0'],
+        )
+
+    @classmethod
+    def setUpClass(cls):
+        logging.default_config()
+
+    def setUp(self):
+        # Disable logging for records module
+        # TODO replace with a mock logger to collect messages
+        logging.getLogger(records.__name__).setLevel(100)
+
+    def test_read_records_from_csv_raw(self):
+        input = io.StringIO(self.text)
+        expected = self.raw_records
+        actual = tuple(records.read_records_from_csv(input))
+        self.assertEqual(expected, actual)
+
+    def test_read_records_from_csv(self):
+        # Only accept records of length 3-5.  Strip fields of
+        # whitespace.
+        def record_constructor(fields):
+            if not (3 <= len(fields) <= 5):
+                raise ValueError("Bad number of fields")
+            return [f.strip() for f in fields]
+        input = io.StringIO(self.text)
+        expected = tuple(
+            [f.strip() for f in self.raw_records[i]]
+            for i in (0, 4, 5, 6, 7, 8))
+        actual = tuple(records.read_records_from_csv(input, record_constructor))
+        self.assertEqual(expected, actual)
