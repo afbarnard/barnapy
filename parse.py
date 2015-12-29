faa392cbdb0c1116a0c04dbd1949e0751a505aa6
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2015-12-29 01:54:39 +0000

    Fixed predicate parsing.

--- parse.py
+++ parse.py
@@ -3,14 +3,19 @@
 import re
 
 
-_predicate_pattern = re.compile(r'\s*(\w+)\s*\((.*)\)\s*')
+_predicate_pattern = re.compile(r'\s*(\w[\w!?@$_-]*)(?:\s*\((.*)\))?\s*')
 _list_split_pattern = re.compile(r'\s*,\s*')
 
 def predicate(text):
-    match = _predicate_pattern.match(text)
+    match = _predicate_pattern.fullmatch(text)
     if match:
-        name, args_text = match.groups((1, 2))
-        args = _list_split_pattern.split(args_text.strip())
+        name, args_text = match.groups()
+        if (args_text is None or
+                len(args_text) == 0 or
+                args_text.isspace()):
+            args = ()
+        else:
+            args = _list_split_pattern.split(args_text.strip())
         return name, args
     else:
         raise ValueError(
@@ -22,9 +27,9 @@ def array_index(text):
     """Parses the text as array indexing syntax (e.g. "a[0]").  Returns
     a (name, index) pair.
     """
-    match = _array_index_pattern.match(text)
+    match = _array_index_pattern.fullmatch(text)
     if match is not None:
-        return match.groups((1, 2))
+        return match.groups()
     else:
         raise ValueError(
             'Could not parse as array and index: {}'.format(text))
