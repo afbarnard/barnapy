3678d453c04b9e5c66e5c76070adbb3b438fa3d4
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2015-12-29 20:41:26 +0000

    Added wrapper for streams and factory method to create files/streams based on arguments.  Fixed spelling of "writable".

--- files.py
+++ files.py
@@ -1,11 +1,21 @@
 # Convenient, high-level file API
 
+import io
 import os.path
 
 
 # TODO how handle streams? can they be effectively wrapped?
 # TODO how handle resolving names of executables?
 # TODO path templates? shell names/tokens/words?
+
+def new(filename_or_stream):
+    if isinstance(filename_or_stream, str):
+        return File(filename_or_stream)
+    elif isinstance(filename_or_stream, io.IOBase):
+        return Stream(filename_or_stream)
+    else:
+        raise TypeError('Not a filename or stream: {}'.format())
+
 
 class File:
     """File paths as objects, like pathlib."""
@@ -69,7 +79,7 @@ class File:
         if not self.is_readable():
             raise ValueError('Not a readable file: {}'.format(self))
 
-    def is_writeable(self):
+    def is_writable(self):
         if os.path.exists(self._path):
             return (os.path.isfile(self._path) and
                     os.access(self._path, os.W_OK, effective_ids=True))
@@ -79,9 +89,9 @@ class File:
                     os.path.isdir(parent) and
                     os.access(parent, os.W_OK, effective_ids=True))
 
-    def assert_writeable(self): # TODO better name? check/assert/?
-        if not self.is_writeable():
-            raise ValueError('Not a writeable file: {}'.format(self))
+    def assert_writable(self): # TODO better name? check/assert/?
+        if not self.is_writable():
+            raise ValueError('Not a writable file: {}'.format(self))
 
     # TODO is_executable
 
@@ -107,3 +117,66 @@ class File:
 
     def __repr__(self):
         return 'File({})'.format(repr(self._path))
+
+
+class Stream:
+
+    def __init__(self, stream, name=None):
+        # Check for subclass of IOBase
+        if not isinstance(stream, io.IOBase):
+            raise TypeError(
+                'Not an instance of IOBase: {!r}'.format(stream))
+        self._stream = stream
+        self._name = name
+        if name is None:
+            if hasattr(stream, 'name'):
+                self._name = stream.name
+            else:
+                self._name = repr(stream)
+
+    @property
+    def name(self):
+        return self._name
+
+    def is_readable(self):
+        return self._stream.readable()
+
+    def assert_readable(self):
+        if not self.is_readable():
+            raise ValueError('Not a readable stream: {}'.format(self))
+
+    def is_writable(self):
+        return self._stream.writable()
+
+    def assert_writable(self):
+        if not self.is_writable():
+            raise ValueError('Not a writable stream: {}'.format(self))
+
+    def open(self, mode):
+        # Check mode is compatible with the existing stream
+        mode = mode.lower()
+        if 'r' in mode:
+            self.assert_readable()
+        if 'w' in mode or 'a' in mode:
+            self.assert_writable()
+        if '+' in mode:
+            self.assert_readable()
+            self.assert_writable()
+        text_stream = isinstance(self._stream, io.TextIOBase)
+        if text_stream and 'b' in mode:
+            raise TypeError(
+                'Text stream not compatible with binary mode: {}'
+                .format(mode))
+        if not text_stream and 't' in mode:
+            raise TypeError(
+                'Binary stream not compatible with text mode: {}'
+                .format(mode))
+        return self._stream
+
+    def generate_lines(self):
+        with self.open('rt') as file:
+            for line in file:
+                yield line
+
+    def __repr__(self):
+        return 'Stream({})'.format(self._name)
