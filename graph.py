7fadb889477921721884175ccb8e3a2722f1e329
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2016-01-14 04:47:16 +0000

    Added methods for degree and bulk construction.  Fixed implementation.
    * Added methods for bulk adding of nodes and edges.
    * Added methods for in / out degree.
    * Fixed bug where adding an edge did not add the involved nodes to the
      node store.
    * Updated to call internal implementation where possible rather than
      repeating code.
    * Changed node and edge stores to wrap their data structures rather than
      subclass.
    * Made API of node and edges stores consistent with Graph.
    * Updated to properly forward Graph methods to node and edge stores.

--- graph.py
+++ graph.py
@@ -13,19 +13,16 @@ class Graph:
     def from_list_def(self, edges):
         for edge in edges:
             if len(edge) == 1 or (len(edge) == 2 and edge[1] is None):
-                self._node_store.add(edge[0])
+                self.add_node(edge[0])
             elif len(edge) == 2:
-                node1, node2 = edge
-                self._node_store.add(node1)
-                self._node_store.add(node2)
-                self._edge_store.add(*edge)
+                self.add_edge(*edge)
             else:
                 raise ValueError(
                     'Not interpretable as an edge: {}'.format(edge))
 
     def to_list_def(self):
-        loners = set(self._node_store)
-        for node1, node2 in self._edge_store:
+        loners = set(self._node_store.nodes())
+        for node1, node2 in self._edge_store.edges():
             loners.discard(node1)
             loners.discard(node2)
             yield (node1, node2)
@@ -33,62 +30,91 @@ class Graph:
             yield (node,)
 
     def has_node(self, node):
-        return self._node_store.has(node)
+        return self._node_store.has_node(node)
 
     def has_edge(self, node1, node2):
-        return self._edge_store.has(node1, node2)
+        return self._edge_store.has_edge(node1, node2)
 
     def num_nodes(self):
-        return len(self._node_store)
+        return self._node_store.num_nodes()
 
     def num_edges(self):
-        return len(self._edge_store)
+        return self._edge_store.num_edges()
 
     def nodes(self):
-        return iter(self._node_store)
+        return self._node_store.nodes()
 
     def edges(self):
-        return iter(self._edge_store)
+        return self._edge_store.edges()
 
     def add_node(self, node):
-        self._node_store.add(node)
+        self._node_store.add_node(node)
 
     def add_edge(self, node1, node2):
-        self._edge_store.add(node1, node2)
+        self._node_store.add_node(node1)
+        self._node_store.add_node(node2)
+        self._edge_store.add_edge(node1, node2)
+
+    def add_nodes(self, nodes):
+        for node in nodes:
+            self.add_node(node)
+
+    def add_edges(self, edges):
+        for node1, node2 in edges:
+            self.add_edge(node1, node2)
+
+    def out_degree(self, node):
+        return self._edge_store.out_degree(node)
+
+    def in_degree(self, node):
+        return self._edge_store.in_degree(node)
 
 
-class SetNodeStore(set): # TODO subclass or wrap?
+class SetNodeStore():
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self):
+        self._nodes = set()
 
-    def has(self, node):
-        return node in self
+    def has_node(self, node):
+        return node in self._nodes
+
+    def num_nodes(self):
+        return len(self._nodes)
 
     def nodes(self):
-        yield from iter(self)
+        return iter(self._nodes)
+
+    def add_node(self, node):
+        self._nodes.add(node)
 
 
-class DictSetEdgeStore(dict): # TODO subclass or wrap?
+class DictSetEdgeStore():
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self):
+        self._parents = {}
 
-    def __iter__(self):
-        return iter(self.edges())
+    def has_edge(self, node1, node2):
+        return node1 in self._parents and node2 in self._parents[node1]
 
-    def has(self, node1, node2):
-        if node1 in self:
-            return node2 in self[node1]
-        else:
-            return False
+    def num_edges(self):
+        return sum(len(children) for children in self._parents.values())
 
     def edges(self):
-        for node, neighbors in self.items():
-            for neighbor in neighbors:
-                yield (node, neighbor)
+        for parent, children in self._parents.items():
+            for child in children:
+                yield (parent, child)
 
-    def add(self, node1, node2):
-        if node1 not in self:
-            self[node1] = set()
-        self[node1].add(node2)
+    def add_edge(self, node1, node2):
+        if node1 not in self._parents:
+            self._parents[node1] = set()
+        self._parents[node1].add(node2)
+
+    def out_degree(self, node):
+        if node in self._parents:
+            return len(self._parents[node])
+        else:
+            return 0
+
+    def in_degree(self, node):
+        return sum(int(node in children)
+                   for children in self._parents.values())
