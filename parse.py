1aac1c2f4cdcb8d49ee5406cb1a916ccd3668b2a
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2016-04-07 21:53:51 +0000

    Added functions for parsing numbers.

--- parse.py
+++ parse.py
@@ -3,6 +3,10 @@
 import re
 
 
+# TODO figure out how to handle options for failed parsing: returning a
+# default value, raising an exception, logging a message (callable with
+# a message), golang paradigm of returning (ok, value) pair
+
 _predicate_pattern = re.compile(r'\s*(\w[\w!?@$_-]*)(?:\s*\((.*)\))?\s*')
 _list_split_pattern = re.compile(r'\s*,\s*')
 
@@ -33,3 +37,36 @@ def array_index(text):
     else:
         raise ValueError(
             'Could not parse as array and index: {}'.format(text))
+
+_integer_pattern = re.compile(r'\s*[+-]?\d+\s*')
+
+_int = int
+def int(text, otherwise=None):
+    match = _integer_pattern.fullmatch(text)
+    if match is not None:
+        return _int(text)
+    elif otherwise is None:
+        return None
+    elif (isinstance(otherwise, type) and
+          issubclass(otherwise, BaseException)):
+        raise otherwise('Could not parse as integer: {}'.format(text))
+    else:
+        return otherwise
+
+# The following float regex is optimized to avoid backtracking
+_exponent_regex = r'[eE][+-]?\d+'
+_float_regex = r'\s*[+-]?(?:\d+(?:\.\d*(?:{0})?|{0})|\.\d+(?:{0})?)\s*'.format(_exponent_regex)
+_float_pattern = re.compile(_float_regex)
+
+_float = float
+def float(text, otherwise=None):
+    match = _float_pattern.fullmatch(text)
+    if match is not None:
+        return _float(text)
+    elif otherwise is None:
+        return None
+    elif (isinstance(otherwise, type) and
+          issubclass(otherwise, BaseException)):
+        raise otherwise('Could not parse as float: {}'.format(text))
+    else:
+        return otherwise
