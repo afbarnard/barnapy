8cb901f1d46ecf8640950912617251221ba56159
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2016-11-28 22:31:49 -0600

    Added functionality for weighted graphs and node / edge deletions.
    * Weights are stored sparsely and a default weight may be set (e.g. 0).
    * Added weight store member to Graph class.
    * Added DictWeightStore implementation.
    * Added ability to sort when dumping graph with `to_nodes_edges` or
      `to_nodes_edges_weights`.

--- graph.py
+++ graph.py
@@ -7,13 +7,23 @@ the MIT license.  See LICENSE for details.
 
 class Graph:
 
-    def __init__(self, node_store=None, edge_store=None):
+    def __init__(
+            self,
+            node_store=None,
+            edge_store=None,
+            weight_store=None,
+            default_weight=None,
+            ):
         self._node_store = (node_store
                             if node_store is not None
                             else SetNodeStore())
         self._edge_store = (edge_store
                             if edge_store is not None
                             else DictSetEdgeStore())
+        self._weight_store = (weight_store
+                              if weight_store is not None
+                              else DictWeightStore())
+        self._default_weight = default_weight
 
     @staticmethod
     def from_nodes_edges(nodes_edges):
@@ -29,17 +39,36 @@ class Graph:
                     .format(item))
         return _graph
 
-    def to_nodes_edges(self):
-        for node in self.nodes():
+    def to_nodes_edges(self, sort=False):
+        nodes = self.nodes()
+        edges = self.edges()
+        if sort:
+            nodes = sorted(nodes)
+            edges = sorted(edges)
+        for node in nodes:
             yield (node,)
-        for edge in self.edges():
+        for edge in edges:
             yield edge
+
+    def to_nodes_edges_weights(self, sort=False):
+        nodes = self.nodes()
+        edges_weights = self.edges_weights()
+        if sort:
+            nodes = sorted(nodes)
+            edges_weights = sorted(edges_weights)
+        for node in nodes:
+            yield (node,)
+        for edge_weight in edges_weights:
+            yield edge_weight
 
     def has_node(self, node):
         return self._node_store.has_node(node)
 
     def has_edge(self, node1, node2):
         return self._edge_store.has_edge(node1, node2)
+
+    def has_weight(self, node1, node2):
+        return self._weight_store.has_weight(node1, node2)
 
     def number_nodes(self):
         return self._node_store.number_nodes()
@@ -53,21 +82,64 @@ class Graph:
     def edges(self):
         return self._edge_store.edges()
 
+    def weight(self, node1, node2):
+        return self._weight_store.weight(
+            node1, node2, self._default_weight)
+
+    def edges_weights(self):
+        for edge in self.edges():
+            yield (edge, self.weight(*edge))
+
     def add_node(self, node):
         self._node_store.add_node(node)
 
-    def add_edge(self, node1, node2):
+    def add_edge(self, node1, node2, weight=None):
         self._node_store.add_node(node1)
         self._node_store.add_node(node2)
         self._edge_store.add_edge(node1, node2)
+        # Only set a weight on the edge if specified
+        if weight is not None:
+            self.set_weight(node1, node2, weight)
+
+    def set_weight(self, node1, node2, weight):
+        # Add edge if it doesn't exist
+        if not self.has_edge(node1, node2):
+            self.add_edge(node1, node2)
+        self._weight_store.set_weight(node1, node2, weight)
 
     def add_nodes(self, nodes):
         for node in nodes:
             self.add_node(node)
 
     def add_edges(self, edges):
-        for node1, node2 in edges:
-            self.add_edge(node1, node2)
+        for edge in edges:
+            self.add_edge(*edge)
+
+    def del_node(self, node):
+        # Delete edges between this node and its neighbors
+        for neighbor in self.in_neighbors(node):
+            self.del_edge(neighbor, node)
+        for neighbor in self.out_neighbors(node):
+            self.del_edge(node, neighbor)
+        # Then delete this node
+        self._node_store.del_node(node)
+
+    def del_edge(self, node1, node2):
+        # Delete the weight, if any
+        if self.has_weight(node1, node2):
+            self.del_weight(node1, node2)
+        self._edge_store.del_edge(node1, node2)
+
+    def del_weight(self, node1, node2):
+        self._weight_store.del_weight(node1, node2)
+
+    def del_nodes(self, nodes):
+        for node in nodes:
+            self.del_node(node)
+
+    def del_edges(self, edges):
+        for edge in edges:
+            self.del_edge(edge)
 
     def out_degree(self, node):
         return self._edge_store.out_degree(node)
@@ -82,7 +154,7 @@ class Graph:
         return self._edge_store.in_neighbors(node)
 
 
-class SetNodeStore():
+class SetNodeStore:
 
     def __init__(self):
         self._nodes = set()
@@ -99,8 +171,11 @@ class SetNodeStore():
     def add_node(self, node):
         self._nodes.add(node)
 
+    def del_node(self, node):
+        self._nodes.remove(node)
 
-class DictSetEdgeStore():
+
+class DictSetEdgeStore:
 
     def __init__(self):
         self._parents = {}
@@ -121,6 +196,9 @@ class DictSetEdgeStore():
             self._parents[node1] = set()
         self._parents[node1].add(node2)
 
+    def del_edge(self, node1, node2):
+        self._parents[node1].remove(node2)
+
     def out_degree(self, node):
         if node in self._parents:
             return len(self._parents[node])
@@ -140,3 +218,21 @@ class DictSetEdgeStore():
     def in_neighbors(self, node):
         return (parent for parent, children in self._parents.items()
                 if node in children)
+
+
+class DictWeightStore:
+
+    def __init__(self):
+        self._edges_weights = {}
+
+    def has_weight(self, node1, node2):
+        return (node1, node2) in self._edges_weights
+
+    def weight(self, node1, node2, default=None):
+        return self._edges_weights.get((node1, node2), default)
+
+    def set_weight(self, node1, node2, weight):
+        self._edges_weights[node1, node2] = weight
+
+    def del_weight(self, node1, node2):
+        del self._edges_weights[node1, node2]
