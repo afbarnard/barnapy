10c460d059b50ffc2879ef5f11c90dbceee9a682
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2017-05-22 22:12:06 -0500

    Wrote function to perform shell pattern matching of filenames (globbing).
    * Wrote function to translate a (basic) shell pattern into a regular
      expression.

--- barnapy/unixutils.py
+++ barnapy/unixutils.py
@@ -6,6 +6,7 @@
 
 import io
 import os
+import re
 
 
 def head(filename, num_lines=10):
@@ -39,3 +40,48 @@ def ls(path='.'):
     walker = os.walk(path)
     _, directories, files = next(walker)
     return (directories, files)
+
+
+_shell_pattern_metacharacter_pattern = re.compile(r'(\*|\?)')
+
+def regex_from_pattern(pattern, case_sensitive=True):
+    """only supports '*' and '?' for now"""
+    # Pass through existing regexs.  (Work around private regex object
+    # type.)
+    if isinstance(pattern,
+                  type(_shell_pattern_metacharacter_pattern)):
+        return pattern
+    # Build regex from shell pattern
+    pieces = _shell_pattern_metacharacter_pattern.split(pattern)
+    # Translate pieces
+    for idx, piece in enumerate(pieces):
+        # Convert shell pattern metacharacters to regexs
+        match = _shell_pattern_metacharacter_pattern.match(piece)
+        if match is not None:
+            if piece == '*':
+                pieces[idx] = '.*'
+            elif piece == '?':
+                pieces[idx] = '.'
+            else:
+                raise ValueError(
+                    'Not a shell pattern metacharacter: {!r}'
+                    .format(piece))
+        # Handle other pieces
+        else:
+            pieces[idx] = re.escape(piece)
+    # Build regex
+    flags = 0
+    if not case_sensitive:
+        flags = re.IGNORECASE
+    return re.compile(''.join(pieces), flags)
+
+
+def glob(filenames, *patterns, case_sensitive=True):
+    # Turn all shell patterns into regexs
+    regexs = [regex_from_pattern(p, case_sensitive) for p in patterns]
+    # Match each filename against each regex
+    for filename in filenames:
+        for regex in regexs:
+            if regex.match(filename) is not None:
+                yield filename
+                break
