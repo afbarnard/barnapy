220ee652a610f02d5f9f1c9af714bbcdb488bf6a
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2018-01-09 20:13:21 -0600

    Bumped package version after parse API updates.
    * Updated copyright year.

--- barnapy/__init__.py
+++ barnapy/__init__.py
@@ -4,7 +4,7 @@ Infrastructure for data processing
 A supplemental standard library with an emphasis on data processing,
 made from the common code collected from my research projects.
 
-Copyright (c) 2017 Aubrey Barnard.  This is free software released under
+Copyright (c) 2018 Aubrey Barnard.  This is free software released under
 the MIT license.  See `LICENSE` for details.
 """
 # Note that the above gets used as the descriptions in `setup.py`.
@@ -12,4 +12,4 @@ the MIT license.  See `LICENSE` for details.
 
 
 # Version
-__version__ = '0.0.4'
+__version__ = '0.0.5'
