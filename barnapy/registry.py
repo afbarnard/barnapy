9a891d95cb4fe86ef35df608e537407b55ce28ff
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2024-09-04 11:50:20 -0500

    Implemented hierarchical registry of derivable values.

---
+++ barnapy/registry.py
@@ -0,0 +1,288 @@
+"""
+Registering objects with keys.  Good for mapping names to objects in
+APIs.
+"""
+
+# Copyright (c) 2024 Aubrey Barnard.
+#
+# This is free software released under the MIT License.  See LICENSE for
+# details.
+
+
+from collections.abc import Callable, Hashable, Iterable
+
+
+class RegistryError(Exception):
+    pass
+
+
+class Entry:
+    """
+    Abstract superclass of registry entries.
+
+    The value of an entry is computed (derived) by combining the value
+    of its parent with its partial value.
+    """
+
+    def __repr__(self) -> str:
+        """
+        Assemble and return a string representation of this registry
+        entry.
+        """
+        return '{}({})'.format(
+            self.__class__.__name__,
+            ', '.join(f'{k.lstrip("_")}={v!r}'
+                      for (k, v) in self.__dict__.items()))
+
+    def parent_key(self) -> Hashable | None:
+        """Return the key of the parent of this registry entry, if any."""
+        return None
+
+    def value(self, registry) -> object:
+        """
+        Return the value of this registry entry, deriving it as
+        necessary from parent values.
+        """
+        raise NotImplementedError('Abstract method')
+
+    def partial_value(self, registry) -> object:
+        """Return the partial value of this registry entry."""
+        raise NotImplementedError('Abstract method')
+
+    def set_partial_value(self, registry, value: object):
+        """Set this registry entry's partial value to the given value."""
+        raise NotImplementedError('Abstract method')
+
+
+class AliasEntry(Entry):
+    """Registry entry that just aliases another registry entry."""
+
+    def __init__(self, parent_key):
+        self._parent_key = parent_key
+
+    def parent_key(self):
+        return self._parent_key
+
+    def value(self, registry):
+        return registry.value(self.parent_key())
+
+    def partial_value(self, registry):
+        return registry.entry(self.parent_key()).partial_value(registry)
+
+    def set_partial_value(self, registry, value):
+        registry.entry(self.parent_key()).set_partial_value(registry, value)
+
+
+class RootEntry(Entry):
+    """
+    Registry entry with no parent, and so whose partial value and
+    derived value are the same.
+    """
+
+    def __init__(self, value):
+        self._value = value
+
+    def value(self, registry):
+        return self._value
+
+    partial_value = value
+
+    def set_partial_value(self, registry, value):
+        self._value = value
+
+
+class DerivedValueEntry(Entry):
+    """
+    Registry entry whose value is derived from the value of its
+    parent.
+    """
+
+    def __init__(
+            self,
+            parent_key: Hashable,
+            partial_value: object,
+            deriver: Callable[[object, object], object],
+    ):
+        self._parent_key = parent_key
+        self._partial_value = partial_value
+        self._deriver = deriver
+
+    def parent_key(self):
+        return self._parent_key
+
+    def value(self, registry):
+        parent_value = registry.value(self.parent_key())
+        return self._deriver(parent_value, self._partial_value)
+
+    def partial_value(self, registry):
+        return self._partial_value
+
+    def set_partial_value(self, registry, value):
+        self._partial_value = value
+
+    @staticmethod
+    def override_value(base: object, override: object) -> object:
+        return override
+
+    @staticmethod
+    def overlay_dict(
+            base: dict,
+            overlay: dict | Iterable[tuple[Hashable, object]],
+            **kwargs,
+    ) -> dict:
+        return dict(base).update(overlay, **kwargs)
+
+    @classmethod
+    def Overrider(clas, parent_key: Hashable, value: object):
+        return clas(parent_key, value, DerivedValueEntry.override_value)
+
+    @classmethod
+    def Overlayer(clas, parent_key: Hashable, value: dict):
+        return clas(parent_key, value, DerivedValueEntry.overlay_dict)
+
+
+class DynamicEntry(DerivedValueEntry):
+    """
+    Registry entry whose value is always recomputed from the value
+    of its parent.
+    """
+
+
+class StaticEntry(DynamicEntry):
+    """Registry entry whose value is only computed once and then saved."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._value = None
+
+    def value(self, registry):
+        if self._value is None:
+            self._value = super().value(registry)
+        return self._value
+
+    def set_partial_value(self, registry, value):
+        super().set_partial_value(registry, value)
+        self._value = None
+
+
+class HierarchicalRegistry:
+    """
+    Register values with keys where values can derive from other
+    values.
+
+    Also acts like a read-only 'dict' that maps keys to values.  (The
+    registry entries are not exposed.)  However, note that values may be
+    (re)computed on each lookup and that each lookup may involve
+    traversing the hierarchy, so lookup should not be considered
+    constant time.
+    """
+
+    def __init__(self, name: str):
+        """Create a hierarchical registry named with the given name."""
+        self._name = name
+        self._reg = dict()
+
+    def n_entries(self) -> int:
+        """Return how many entries this registry contains."""
+        return len(self._reg)
+
+    def has_key(self, key: Hashable) -> bool:
+        """Whether this registry has the given key."""
+        return key in self._reg
+
+    def entry(self, key: Hashable) -> Entry:
+        """Return the entry registered with the given key."""
+        return self._reg[key]
+
+    def parent_key(self, key: Hashable) -> Hashable | None:
+        """Return the parent key of the given key, if any."""
+        return self.entry(key).parent_key()
+
+    def path(self, key: Hashable) -> list:
+        """
+        Return the "path" of the given key.
+
+        Like a filesystem path, the path is the list of keys that lead
+        to the given key from its root.
+        """
+        keys = []
+        while key is not None:
+            parent = self.parent_key(key)
+            keys.append(key)
+            key = parent
+        keys.reverse()
+        return keys
+
+    def value(self, key: Hashable) -> object:
+        """
+        Compute and return the value registered with the given key.
+
+        Note that computing the value may involve looking up each parent
+        and computing their value recursively until the root.
+        """
+        return self.entry(key).value(self)
+
+    def register(self, key: Hashable, entry: Entry):
+        """
+        Register the given entry with the given key.
+
+        The key must not be 'None', must not already be registered, and
+        the parent of the entry must already exist in the registry (or
+        be 'None' in which case it's a root).
+        """
+        if key is None:
+            raise RegistryError("'None' is not allowed as a registry key")
+        if key in self._reg:
+            raise RegistryError(f'Key already registered: {key!r}')
+        parent = entry.parent_key()
+        if parent is not None and parent not in self._reg:
+            raise RegistryError(
+                f'Parent of key {key!r} does not exist: {parent!r}')
+        self._reg[key] = entry
+        return self
+
+    def register_all(self, keys_entries: Iterable[tuple[Hashable, Entry]]):
+        for (key, entry) in keys_entries:
+            self.register(key, entry)
+        return self
+
+    def __repr__(self) -> str:
+        return '{}({!r}, {!r})'.format(
+            self.__class__.__name__, self._name, self._reg)
+
+    # Partial (read-only) 'dict' interface
+
+    __len__ = n_entries
+
+    def __iter__(self):
+        """Return an iterator over this registry's keys."""
+        return iter(self._reg)
+
+    __contains__ = has_key
+
+    __getitem__ = value
+
+    def get(self, key: Hashable, default: object=None) -> object:
+        """
+        Get the value for the given key if it is registered else
+        return 'default'.
+        """
+        return self.value(key) if self.has_key(key) else default
+
+    def keys(self):
+        """Return a set-like view of this registry's keys."""
+        return self._reg.keys()
+
+    def values(self):
+        """
+        Return an iterator over values, recomputing them as
+        necessary.
+        """
+        return (self.value(k) for k in self.keys())
+
+    def items(self):
+        """
+        Return an iterator over (key, value) pairs, recomputing
+        values as necessary.
+        """
+        return ((k, self.value(k)) for k in self.keys())
---
+++ barnapy/test/registry_test.py
@@ -0,0 +1,228 @@
+"""Tests 'registry.py'."""
+
+# Copyright (c) 2024 Aubrey Barnard.
+#
+# This is free software released under the MIT License.  See LICENSE for
+# details.
+
+
+import unittest
+
+from .. import registry
+
+
+def _mk_cat(sep=''):
+    def _cat(x1, x2):
+        return f'{x1}{sep}{x2}'
+    return _cat
+
+
+class RootEntryTest(unittest.TestCase):
+
+    def setUp(self):
+        value = '_the_ value'
+        entry = registry.RootEntry(value)
+        reg = registry.HierarchicalRegistry('root entry').register_all([
+            ('root', entry),
+        ])
+        self.value = value
+        self.entry = entry
+        self.reg = reg
+
+    def test_value(self):
+        self.assertEqual(self.value, self.reg.value('root'))
+
+    def test_partial_value(self):
+        self.assertEqual(
+            self.value, self.reg.entry('root').partial_value(self.reg))
+
+    def test_set_partial_value(self):
+        value = 'the _new_ value'
+        self.entry.set_partial_value(self.reg, value)
+        self.assertEqual(value, self.reg.entry('root').partial_value(self.reg))
+
+
+class AliasEntryTest(unittest.TestCase):
+
+    def setUp(self):
+        value = '_the_ value'
+        reg = registry.HierarchicalRegistry('alias entry').register_all([
+            ('root', registry.RootEntry(value)),
+            ('base', registry.AliasEntry('root')),
+            ('init', registry.AliasEntry('base')),
+            ('seed', registry.AliasEntry('init')),
+        ])
+        self.value = value
+        self.reg = reg
+
+    def test_value(self):
+        self.assertEqual(self.value, self.reg.value('seed'))
+
+    def test_partial_value(self):
+        self.assertEqual(
+            self.value, self.reg.entry('seed').partial_value(self.reg))
+
+    def test_set_partial_value(self):
+        value = 'the _new_ value'
+        self.reg.entry('seed').set_partial_value(self.reg, value)
+        self.assertEqual(value, self.reg.entry('root').partial_value(self.reg))
+        self.assertEqual(value, self.reg.value('seed'))
+
+
+class DynamicEntryTest(unittest.TestCase):
+
+    def setUp(self):
+        _cat = _mk_cat()
+        self.reg = registry.HierarchicalRegistry('dynamic entry').register_all([
+            ('a', registry.RootEntry('a')),
+            ('b', registry.DynamicEntry('a', 'b', _cat)),
+            ('c', registry.DynamicEntry('b', 'c', _cat)),
+            ('d', registry.DynamicEntry('c', 'd', _cat)),
+            ('e', registry.DynamicEntry('d', 'e', _cat)),
+        ])
+
+    def test_value(self):
+        chars = 'abcde'
+        for (idx, char) in enumerate(chars):
+            self.assertEqual(chars[:(idx + 1)], self.reg.value(char))
+
+    def test_partial_value(self):
+        for char in 'abcde':
+            self.assertEqual(
+                char, self.reg.entry(char).partial_value(self.reg))
+
+    def test_set_partial_value(self):
+        self.reg.entry('a').set_partial_value(self.reg, 'A')
+        self.assertEqual('Abcde', self.reg.value('e'))
+        self.reg.entry('c').set_partial_value(self.reg, 'C')
+        self.assertEqual('AbCde', self.reg.value('e'))
+        self.reg.entry('e').set_partial_value(self.reg, 'E')
+        self.assertEqual('AbCdE', self.reg.value('e'))
+
+
+class StaticEntryTest(unittest.TestCase):
+
+    def setUp(self):
+        _cat = _mk_cat()
+        self.reg = registry.HierarchicalRegistry('static entry').register_all([
+            ('a', registry.RootEntry('A')),
+            ('b', registry.StaticEntry('a', 'B', _cat)),
+            ('c', registry.StaticEntry('b', 'C', _cat)),
+            ('d', registry.StaticEntry('c', 'D', _cat)),
+            ('e', registry.StaticEntry('d', 'E', _cat)),
+        ])
+
+    def test_value(self):
+        keys = 'abcde'
+        vals = keys.upper()
+        # First the value is computed
+        for (idx, key) in enumerate(keys):
+            self.assertEqual(vals[:(idx + 1)], self.reg.value(key))
+        # Then it is retrieved from saved
+        for (idx, key) in enumerate(keys):
+            self.assertEqual(vals[:(idx + 1)], self.reg.value(key))
+
+    def test_set_partial_value(self):
+        keys = 'abcde'
+        vals = keys.upper()
+        vals_new = vals.lower()
+        # Compute all values
+        for key in keys:
+            self.reg.value(key)
+        # Lowercase values one by one
+        for (idx, key) in enumerate(keys[:-1]):
+            self.reg.entry(key).set_partial_value(self.reg, vals[idx].lower())
+            self.assertEqual(vals_new[:(idx + 1)], self.reg.value(key))
+            self.assertEqual(vals[:(idx + 2)], self.reg.value(keys[idx + 1]))
+
+
+class HierarchicalRegistryTest(unittest.TestCase):
+
+    def test_basics(self):
+        _dot_cat = _mk_cat('.')
+        reg = registry.HierarchicalRegistry('basics').register_all([
+            ('r', registry.RootEntry('R')),
+            ('l1', registry.StaticEntry('r', 'L1', _dot_cat)),
+            ('l2', registry.StaticEntry('r', 'L2', _dot_cat)),
+        ])
+        self.assertEqual(3, len(reg))
+        self.assertIn('l2', reg)
+        self.assertNotIn('l3', reg)
+        self.assertEqual('r', reg.parent_key('l1'))
+        self.assertEqual('R.L2', reg['l2'])
+        self.assertEqual(['r', 'l1', 'l2'], list(reg))
+        self.assertEqual(['r', 'l1', 'l2'], list(reg.keys()))
+        self.assertEqual(['R', 'R.L1', 'R.L2'], list(reg.values()))
+        self.assertEqual([('r', 'R'), ('l1', 'R.L1'), ('l2', 'R.L2')],
+                         list(reg.items()))
+
+    def test_get(self):
+        reg = registry.HierarchicalRegistry('get').register_all([
+            ('r', registry.RootEntry('R')),
+        ])
+        self.assertEqual('R', reg.get('r', 'Q'))
+        self.assertEqual('Q', reg.get('s', 'Q'))
+
+    def test_path(self):
+        _dot_cat = _mk_cat('.')
+        reg = registry.HierarchicalRegistry('path').register_all([
+            ('a', registry.RootEntry('A')),
+            ('b', registry.DynamicEntry('a', 'B', _dot_cat)),
+            ('c', registry.DynamicEntry('b', 'C', _dot_cat)),
+            ('d', registry.DynamicEntry('c', 'D', _dot_cat)),
+            ('e', registry.DynamicEntry('d', 'E', _dot_cat)),
+        ])
+        keys = 'abcde'
+        for (idx, key) in enumerate(keys):
+            self.assertEqual(list(keys[:(idx + 1)]), reg.path(key))
+
+    def test_register(self):
+        reg = registry.HierarchicalRegistry('register')
+        self.assertEqual(0, len(reg))
+        self.assertNotIn('r', reg)
+        reg.register('r', registry.RootEntry('R'))
+        self.assertEqual(1, len(reg))
+        self.assertIn('r', reg)
+        self.assertEqual('R', reg['r'])
+
+    def test_register__errors(self):
+        reg = registry.HierarchicalRegistry('register')
+        with self.assertRaises(registry.RegistryError):
+            reg.register(None, registry.RootEntry('R'))
+        reg.register('r', registry.RootEntry('R'))
+        with self.assertRaises(registry.RegistryError):
+            reg.register('r', registry.RootEntry('R'))
+        with self.assertRaises(registry.RegistryError):
+            reg.register('s', registry.DynamicEntry.Overrider('t', 'S'))
+
+    def test_derivation_w_all_entries(self):
+        _dot_cat = _mk_cat('.')
+        reg = registry.HierarchicalRegistry('all entries').register_all([
+            ('l0r1', registry.RootEntry('L0R1')),
+            ('al0r1', registry.AliasEntry('l0r1')),
+            ('l1d1', registry.DynamicEntry('al0r1', 'L1D1', _dot_cat)),
+            ('al1d1', registry.AliasEntry('l1d1')),
+            ('l2s1', registry.StaticEntry('al1d1', 'L2S1', _dot_cat)),
+            ('al2s1', registry.AliasEntry('l2s1')),
+            ('l0r2', registry.RootEntry('L0R2')),
+            ('al0r2', registry.AliasEntry('l0r2')),
+            ('l1s2', registry.StaticEntry('al0r2', 'L1S2', _dot_cat)),
+            ('al1s2', registry.AliasEntry('l1s2')),
+            ('l2d2', registry.DynamicEntry('al1s2', 'L2D2', _dot_cat)),
+            ('al2d2', registry.AliasEntry('l2d2')),
+        ])
+        self.assertEqual('L0R1.L1D1.L2S1', reg['al2s1'])
+        self.assertEqual('L0R2.L1S2.L2D2', reg['al2d2'])
+        reg.entry('al0r1').set_partial_value(reg, './L0R1')
+        reg.entry('al0r2').set_partial_value(reg, './L0R2')
+        self.assertEqual('./L0R1.L1D1', reg['al1d1'])
+        self.assertEqual('L0R1.L1D1.L2S1', reg['al2s1'])
+        self.assertEqual('L0R2.L1S2.L2D2', reg['al2d2'])
+        reg.entry('al2d2').set_partial_value(reg, '↑L2D2')
+        self.assertEqual('L0R2.L1S2.↑L2D2', reg['al2d2'])
+        reg.entry('al1d1').set_partial_value(reg, '↑L1D1')
+        self.assertEqual('./L0R1.↑L1D1', reg['al1d1'])
+        reg.entry('al2s1').set_partial_value(reg, '←L2S1')
+        self.assertEqual('./L0R1.↑L1D1.←L2S1', reg['al2s1'])
+        reg.entry('al1s2').set_partial_value(reg, '←L1S2')
+        self.assertEqual('./L0R2.←L1S2.↑L2D2', reg['al2d2'])
